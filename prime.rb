670f991d954520ba57543b413200f20da054ed5a
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-06 00:52:18 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,2 +1,17 @@
-def prime?(num)
-end
+ def is_prime n
+  for d in 2..(n - 1)
+   if (n % d) == 0
+    return false
+   end
+  end
+
+  true
+ end
+
+ def is_prime2 n
+  foundDivider = false
+   for d in 2..(n - 1)
+    foundDivider = ((n % d) == 0) or foundDivider
+   end
+  not foundDivider
+ end
