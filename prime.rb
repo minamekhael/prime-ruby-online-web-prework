251ded04ea77043164e1fe24c70d1b10b18d7e2c
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-06 00:53:38 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,2 @@
-def get_prime_no_upto(number)
-  start = 2
-  primes = (start..number).to_a
-  (start..number).each do |no|
-    (start..no).each do |num|
-      if ( no % num  == 0) && num != no
-        primes.delete(no)
-        break
-      end
-    end
-  end
-  primes
-end
+def prime?(int)
+  
