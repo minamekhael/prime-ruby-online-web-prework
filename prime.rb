9928d547aa1e3d3a8cd22c3b0611be3692352881
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-06 01:01:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,2 @@
 def prime_numbers(n)
-  primes = []
-  i = 2
-  while @primes.size < n do
-    @primes << i if is_prime?(i)
-    i += 1
-  end
-  @primes
-end
-
-def is_prime?(n)
-  @primes.each { |prime| return false if n % prime == 0 }
-  true
-end
+  if 
