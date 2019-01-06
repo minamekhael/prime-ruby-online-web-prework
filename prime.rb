f640c06d279f5834b75b09c2fc169c77e36b3583
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-06 01:06:23 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,9 @@
 def prime?(int)
-  # To determine whether int is a prime number returned false if less than 2
     return false if int < 2
-  
     (2..int - 1).each do |x|
         if (int % x) == 0
             return false
         end
-    end
+        end
     true
 end
